839167028df3d2120cfac684a76f67801538d31e
SHIVAM KESHARI <149083540+SHIVAMKESHARI23@users.noreply.github.com>
2025-12-10 22:21:39 +0530

    Time: 0 ms (100%), Space: 92.8 MB (47%) - LeetHub

---
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPermutations(vector<int>& complexity) {
+        const int MOD = 1000000007;
+        int n = complexity.size();
+        
+        for (int i = 1; i < n; i++) {
+            if (complexity[i] <= complexity[0]) return 0;
+        }
+        
+        long long ans = 1;
+        for (int i = 1; i < n; i++) {
+            ans = (ans * i) % MOD;
+        }
+        
+        return ans;
+    }
+};
