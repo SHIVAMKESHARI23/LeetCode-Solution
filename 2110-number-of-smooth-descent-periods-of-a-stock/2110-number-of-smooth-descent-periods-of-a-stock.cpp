8b0aa832cd71adb7851a74ecfd4bf5c341301aa4
SHIVAM KESHARI <149083540+SHIVAMKESHARI23@users.noreply.github.com>
2025-12-15 23:13:15 +0530

    Time: 0 ms (100%), Space: 107.2 MB (14.5%) - LeetHub

---
+++ 2110-number-of-smooth-descent-periods-of-a-stock/2110-number-of-smooth-descent-periods-of-a-stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        long long ans = 0;
+        long long len = 1;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i - 1] - prices[i] == 1) {
+                len++;
+            } else {
+                len = 1;
+            }
+            ans += len;
+        }
+        return ans + 1;
+    }
+};
