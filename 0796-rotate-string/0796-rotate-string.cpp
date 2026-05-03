e13394fda683c0b36ee612d0cf6417b1fbdc48fd
SHIVAM KESHARI <149083540+SHIVAMKESHARI23@users.noreply.github.com>
2026-05-03 11:04:25 +0530

    Time: 3 ms (6.54%), Space: 8.2 MB (61.66%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,30 +1,7 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.size()!=goal.size()){
-            return false;
-        }
-        int index=0;
-        int i=0;
-        string ans="";
-        while(index<s.size()){
-            ans+=s[(i+1)%s.size()];
-            i++;
-            if(i==s.size()){
-                if(ans==goal){
-                    return true;
-                }
-                
-                    s=ans;
-                    ans="";
-
-                    i=0;
-                    index++;
-
-                }
-
-
-        }
-        return false;
+        if (s.size() != goal.size()) return false;
+        return (s + s).find(goal) != string::npos;
     }
 };
