82a80adbb60e3e9d119a1a90ce7038487ab42922
SHIVAM KESHARI <149083540+SHIVAMKESHARI23@users.noreply.github.com>
2026-08-21 23:32:50 +0530

    Time: 0 ms (100%), Space: 17.7 MB (26.6%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,48 +1,56 @@
 class Solution {
-    private:
-    bool dfs(vector<int>&color,int i,vector<vector<int>>&adj){
-        stack<int>st;
-        st.push(i);
-        color[i]=1;
-        while(!st.empty()){
-            int node=st.top();
-            st.pop();
-            for(int j=0;j<adj[node].size();j++){
-                int nr=adj[node][j];
-                if(color[nr]==0){
-                    if(color[node]==1){
-                        color[nr]=2;
+private:
+    bool bfs(int start, vector<int>& color, vector<vector<int>>& graph) {
+        queue<int> qu;
+
+        qu.push(start);
+        color[start] = 1;
+
+        while (!qu.empty()) {
+            int node = qu.front();
+            qu.pop();
+
+            for (int neighbour : graph[node]) {
+
+                if (color[neighbour] == 0) {
+
+                    if (color[node] == 1) {
+                        color[neighbour] = 2;
                     }
-                    else{
-                        // color[node]==2;
-                        color[nr]=1;
+                    else {
+                        color[neighbour] = 1;
                     }
-                    st.push(nr);
+
+                    qu.push(neighbour);
                 }
                 else {
-                    if(color[node]==color[nr])return false;
+                    if (color[node] == color[neighbour]) {
+                        return false;
+                    }
                 }
             }
         }
+
         return true;
     }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        vector<vector<int>>adj(graph.size());
-        for(int i=0;i<graph.size();i++){
-            for(int j=0;j<graph[i].size();j++){
 
-            adj[i].push_back(graph[i][j]);
+        int n = graph.size();
+
+        vector<int> color(n, 0);
+
+        for (int i = 0; i < n; i++) {
+
+            if (color[i] == 0) {
+
+                if (!bfs(i, color, graph)) {
+                    return false;
+                }
             }
         }
-        int n=graph.size();
-        vector<int>color(n,0);
 
-        for(int i=0;i<n;i++){
-            if(color[i]==0){
-                if(!dfs(color,i,adj)) return false;
-            }
-        }
         return true;
     }
 };
